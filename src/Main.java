c74c59e58be5762f08cf39cd7894e0de80372238
Darshit Dhaduk <darshitdhaduk129@gmail.com>
2026-02-12 23:32:10 +0530

    some task done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
-class A{
-    public A(){
-
-    }
-}
-class B extends A{
-    public B(){
-    }
-    public B(int a,int b){
-        this();
-    }
-}
 class Main{
+    public static void sayHello(){
+        System.out.println("Hello World!");
+    }
     public static void main(String[] args) {
+        int []arr = {1,2,3};
 
-        B b = new B();
+        for(int i = 0;i<10;i++)
+        {
+            sayHello();
+        }
+        System.out.println("Done");
     }
 }
